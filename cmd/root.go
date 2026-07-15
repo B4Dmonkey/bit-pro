e3551afc43a5bfc3547cf4fd971af3628d2ac944
josiah <lowgen0@gmail.com>
2026-07-15 12:15:02 -0400

    feat(bootstrap): add bit init command to create .bit/

--- cmd/root.go
+++ cmd/root.go
@@ -5,9 +5,11 @@ import "github.com/spf13/cobra"
 const version = "0.1.0-dev"
 
 func NewRootCmd() *cobra.Command {
-	return &cobra.Command{
+	rootCmd := &cobra.Command{
 		Use:     "bit",
 		Short:   "bit is a project-management CLI for LLM-driven development workflows",
 		Version: version,
 	}
+	rootCmd.AddCommand(newInitCmd())
+	return rootCmd
 }
